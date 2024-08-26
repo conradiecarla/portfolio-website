2fb08eaae56571c215ae35b3738299e68b1dbf94
conradiecarla <conradiecarla@gmail.com>
2024-08-26 16:22:21 +0100

    Menu Changes with JQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
-
 console.log("Hi! Welcome to my portfolio site.")
+
+var escapedText2 = 'He said: "Yes!", that\'s for sure';
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.classame = 'navtoggle';
+  }
+}
